0cfbc1af928b0f7a0b0af56bdad4d9e638eb6774
Your Name <you@example.com>
2023-11-03 18:36:22 +0200

    merge_tables

--- resources/views/layout/myApp.blade.php
+++ resources/views/layout/myApp.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="ajax-search" content="{{route('ajax.search')}}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/14.6.4/nouislider.min.css">
